ea188eab5b58dd8b6425c4703870e3e752db896f
Aiden Joo <hj356@cornell.edu>
2024-08-02 22:11:39 +0800

    initialized spring boot project

---
+++ src/main/java/com/steaksandwich/steak_sandwich_backend/SteakSandwichBackendApplication.java
@@ -0,0 +1,13 @@
+package com.steaksandwich.steak_sandwich_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteakSandwichBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteakSandwichBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steaksandwich/steak_sandwich_backend/SteakSandwichBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steaksandwich.steak_sandwich_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteakSandwichBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
