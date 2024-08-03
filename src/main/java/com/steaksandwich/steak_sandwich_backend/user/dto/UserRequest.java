50ea52d6f24b21888123c47cbd30de2082181245
wesleyj25ng <wesleyj25ng@gmail.com>
2024-08-03 16:38:57 +0900

    aiden g

--- src/main/java/com/steaksandwich/steak_sandwich_backend/user/dto/UserRequest.java
+++ src/main/java/com/steaksandwich/steak_sandwich_backend/user/dto/UserRequest.java
@@ -2,4 +2,19 @@ package com.steaksandwich.steak_sandwich_backend.user.dto;
 
 public class UserRequest {
 
+    private String username;
+    private String email;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
--- src/main/java/com/steaksandwich/steak_sandwich_backend/user/dto/UserResponse.java
+++ src/main/java/com/steaksandwich/steak_sandwich_backend/user/dto/UserResponse.java
@@ -1,5 +1,15 @@
 package com.steaksandwich.steak_sandwich_backend.user.dto;
 
+import com.steaksandwich.steak_sandwich_backend.user.entity.User;
+
 public class UserResponse {
 
+    private Long id;
+    private String username;
+    private String password;
+    private int points;
+
+    public UserResponse(User user) {
+        this.id = user.getId()
+    }
 }
